b8390d57149b218f37a567ec5d9213d023870ef2
nicolas-limadev <nicolasbasto76@gmail.com>
2023-08-09 19:09:05 -0300

    terraform-correction

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-tfstate-live"
+    bucket = "terraform-tfstate-nicklima"
     key = "prd/live-api-site/terraform.tfstate"
     region = "us-east-1"
   }
